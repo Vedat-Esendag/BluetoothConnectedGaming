d9dd9d97e776b28d4cff18efb1c837eadfcf4b6f
faikzaim <xj22509@bristol.ac.uk>
2026-06-15 02:11:37 +0100

    Fix formatting issue.

--- lib/core/bluetooth_service.dart
+++ lib/core/bluetooth_service.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -7,14 +6,13 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 class BluetoothService {
   // Singleton design pattern
   BluetoothService._();
+
   static final BluetoothService instance = BluetoothService._();
 
   Future<void> initialise() async {
-
     // first, check if bluetooth is supported by your hardware
     // Note: The platform is initialized on the first call to any FlutterBluePlus method.
     if (!(await FlutterBluePlus.isSupported)) {
-      //TODO: Send signal to UI: Display a signal informing the user the app is not compatible on their device
       print('Bluetooth not supported by this device');
       return;
     }
@@ -24,8 +22,5 @@ class BluetoothService {
     if (!kIsWeb && Platform.isAndroid) {
       await FlutterBluePlus.turnOn();
     }
-
-    return;
   }
-
 }
